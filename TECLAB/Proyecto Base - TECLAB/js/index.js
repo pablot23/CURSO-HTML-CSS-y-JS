14acec02e8894f5e1f560d2da64128cfb28bd13a
pablot23 <plablot@fibertel.com.ar>
2025-06-04 20:52:55 -0300

    Proyecto_base

---
+++ TECLAB/Proyecto Base - TECLAB/js/index.js
@@ -0,0 +1,12 @@
+let nombre="teclab"
+let fechaNacimiento="21/03/1975"
+let anioInicial=2018
+
+const USUARIO="Joe McMillan"
+const SecurityID='044b429d-911a-a5b1d9b7efbd'
+
+const paisesDelSur=['Argentina','Uruguay','Brasil','Venezuela','Chile']
+
+const carrito=[{codigo:123,nombre:'tecla bluetooth',importe:15500},
+{codigo:234,nombre:'Mouse bluetooth',importe:7800},
+{codigo:345,nombre:'SSD Portatil',importe:47350}]
